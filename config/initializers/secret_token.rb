3f6c90717b525765ba106b6ec129ed8fee943870
David Lesches <david@lesches.com>
2012-11-21 22:04:33 -0500

    Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drunkonrails::Application.config.secret_token = '1b24a2d1b920d96769457d968cd8f2d423944158ccb51adb21ebbd845c8f3439fc3bb79c577bc537c748b0a2dbae801be7d67b2045c2c56a33650d77977d9c06'
